11a254b388f2fd239db7ff45aa4febb7124e675e
tommyrharper <thomasrobertharper@gmail.com>
2021-07-13 01:35:48 +0100

    Save atomiql code

---
+++ projects/production/code_bits/get_query_ast.ts
@@ -0,0 +1,16 @@
+
+
+    // typeDefs.definitions.forEach((def) => {
+    //   if (isObjectTypeDefinitionNode(def)) {
+    //     if (def.name.value === 'Query') {
+    //       (def.fields || []).forEach((field) => {
+    //         if (field.name.value === 'pets') {
+    //           // console.log(`field`, field)
+    //           // field.type.type.name.value
+    //           console.log(`field.type.type.type.name.value`, field.type.type)
+    //           console.log(`field.type.type.type.name.value`, field.type.type.type.name.value)
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
